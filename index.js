03d09c772e50d37c3d98944fc217ea670014b3f1
jeffreysbrother <jeffreysbrother@gmail.com>
2020-08-05 15:45:12 -0700

    refactor using promises; last tag still not appending

--- index.js
+++ index.js
@@ -3,95 +3,132 @@ let jsdom = require("jsdom");
 let fs = require("fs");
 const { JSDOM } = jsdom;
 
-const sitemapName = 'sitemap.xml';
+// TODO 
+// please wait
+// formatting
+// multiple sitemaps
+// log stuff on completion
+//    * # of sitemaps created
+//    * sitemap name, # of entries for each
+
+const sitemapPrefix = 'icm-ppl-pdnames-sitemap-';
+const xmlFile       = `${sitemapPrefix}.xml`;
 
 const sitemapHeader = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 const sitemapFooter = '\n</urlset>';
+const wrapStart     = '\n\t<url>\n\t\t<loc>';
+const wrapEnd       = '</loc>\n\t</url>';
 
-const wrapStart = `\n<url>
-\t<loc>`;
-const wrapEnd = `</loc>
-</url>`;
+// const letters       = [...'abcdefghijklmnopqrstuvwxyz'];
+const letters       = [...'ab'];
+let lastNameURLS    = [];
 
-// if sitemap already exists, delete it
-if (fs.existsSync(sitemapName)) {
-    fs.unlinkSync(sitemapName);
+let c = new Crawler();
+
+function initialize() {
+    return new Promise((resolve, reject) => {
+        // if sitemap already exists, delete it.
+        if (fs.existsSync(xmlFile)) {
+            fs.unlinkSync(xmlFile);
+        }
+
+        // create empty file
+        fs.openSync(xmlFile, 'w');
+
+        fs.appendFile(xmlFile, sitemapHeader, function () {
+            resolve();
+        });
+
+    });
 }
-
-let stream = fs.createWriteStream(sitemapName);
-stream.write(sitemapHeader);
- 
-var c = new Crawler();
-// const letters = [...'abcdefghijklmnopqrstuvwxyz'];
-const letters = [...'abc'];
-let lastNameURLS = [];
 
 
 // crawl letter pages to retrieve last name URLs and add these to an array
-let tcgCrawl = new Promise((resolve, reject) => {
-    letters.forEach((el, index, array) => {
-        c.queue([{
-            uri: `https://www.instantcheckmate.com/people/${el}/`,
-            jQuery: false,
-            rateLimit: 2000,
-            maxConnections: 26,
-         
-            callback: function (error, res, done) {
-        
-                if (error) {
-                    console.log(error);
-                } else {
-        
-                    let dom = new JSDOM(res.body);
-        
-                    dom.window.document.querySelectorAll(".bc-a").forEach(el => {
-                        lastNameURLS.push(`https://www.instantcheckmate.com${el.getAttribute('href')}`);
-                    });
-                    
-                }
-                done();
+function initialCrawl() {
+    return new Promise((resolve, reject) => {
+        letters.forEach((letter, index, array) => {
+            c.queue([{
+                uri: `https://www.instantcheckmate.com/people/${letter}/`,
+                jQuery: false,
+                rateLimit: 2000,
+                maxConnections: 26,
+            
+                callback: function (error, res, done) {
+            
+                    if (error) {
+                        console.log(error);
+                    } else {
+            
+                        let dom = new JSDOM(res.body);
+            
+                        dom.window.document.querySelectorAll(".bc-a").forEach(path => {
+                            lastNameURLS.push(`https://www.instantcheckmate.com${path.getAttribute('href')}`);
+                        });
+                        
+                    }
 
-                // resolve promise when forEach iteration is complete
-                if (index === array.length - 1) {
-                    resolve();
-                }
-            }
+                    done();
 
-        }]);
+                    // resolve promise when forEach iteration is complete
+                    if (index === array.length - 1) {
+                        resolve();
+                    }
+                }
+
+            }]);
+        });
     });
-});
+}
 
 
 // crawl last name pages and write result page URLs to sitemap
-tcgCrawl.then(() => {
-    lastNameURLS.forEach((el, index, array) => {
-        c.queue([{
-            uri: `${el}`,
-            jQuery: false,
-            rateLimit: 2000,
-            maxConnections: 26,
-         
-            callback: function (error, res, done) {
-        
-                if (error) {
-                    console.log(error);
-                } else {
-        
-                    let dom = new JSDOM(res.body);
-        
-                    dom.window.document.querySelectorAll(".bc-a").forEach(el => {
-                        stream.write(`${wrapStart}https://www.instantcheckmate.com${el.getAttribute('href')}${wrapEnd}`);
-                    });
-                    
-                }
-                done();
+function secondCrawl() {
+    return new Promise ((resolve, reject) => {
+        lastNameURLS.forEach((el, i, arr) => {
+            c.queue([{
+                uri: `${el}`,
+                jQuery: false,
+                rateLimit: 2000,
+                maxConnections: 26,
+            
+                callback: function (error, res, done) {
+            
+                    if (error) {
+                        console.log(error);
+                    } else {
+            
+                        let dom = new JSDOM(res.body);
+            
+                        dom.window.document.querySelectorAll(".bc-a").forEach((path, ind, ar) => {
+                            fs.appendFileSync(xmlFile, `${wrapStart}https://www.instantcheckmate.com${path.getAttribute('href')}${wrapEnd}`);
+                        });
+                        
+                    }
 
-                // append final XML tag to file
-                if (index === array.length - 1) {
-                    stream.write(sitemapFooter);
-                }
-            }
+                    done();
 
-        }]);
+                    // resolve promise when forEach iteration is complete
+                    if (i === arr.length - 1) {
+                        resolve();
+                    }
+
+                }
+
+            }]);
+        });
     });
-});
+}
+
+function lastStep() {
+    return new Promise ((resolve, reject) => {
+        fs.appendFile(xmlFile, sitemapFooter, function () {
+            resolve();
+        })
+    });
+}
+
+initialize()
+    .then(() => initialCrawl())
+    .then(() => secondCrawl())
+    .then(() => lastStep())  // append final XML tag to file. this is not working
+    .then(() => console.log('complete'));
